3e80988ee882e1149b2531cf28868e2685fd296c
kevin-r-m <kmclaughlinbtb@gmail.com>
2025-06-23 12:01:12 -0500

    Add tanstack devtools and eslint plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { Container } from '@mui/material';
 import CompetitorManagement from './features/CompetitorManagement';
 import Header from './features/Header';
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <Header />
             <Container>
                 <CompetitorManagement />
             </Container>
-        </>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
